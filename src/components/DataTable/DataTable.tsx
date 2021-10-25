053f9e67b1d2c18ac9c66bab6218e5036cbd09a4
iForced <iforce@2ch.hk>
2021-10-25 22:24:15 +0300

    need to fix route after init app

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -12,6 +12,7 @@ const DataTable = () => {
             <nav className={s.navbar}>
                 <NavLink exact to={'/'} className={s.navLink} activeClassName={s.activeLink}>Сегодня</NavLink>
                 <NavLink exact to={'/all'} className={s.navLink} activeClassName={s.activeLink}>Все дни</NavLink>
+                Выбери вкладку (потом пофикшу)
             </nav>
             <Paper elevation={8} className={s.outputs} style={{backgroundColor: '#3f51b545'}}>
                 <Route path={'/'} exact render={() =>
