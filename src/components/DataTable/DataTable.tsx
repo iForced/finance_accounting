aaa71a0ff0f76930b1dd3f7cf1fe8639b72cf7ce
iForced <iforce@2ch.hk>
2021-10-26 15:34:53 +0300

    fix defaultrouting

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './DataTable.module.css'
 import {Paper} from "@material-ui/core";
-import {NavLink, Route} from "react-router-dom";
+import {NavLink, Redirect, Route} from "react-router-dom";
 import Today from "./Today/Today";
 import AllDays from "./AllDays/AllDays";
 
@@ -10,15 +10,15 @@ const DataTable = () => {
     return (
         <div className={s.table}>
             <nav className={s.navbar}>
-                <NavLink exact to={'/'} className={s.navLink} activeClassName={s.activeLink}>Сегодня</NavLink>
+                <NavLink to={'/today'} className={s.navLink} activeClassName={s.activeLink}>Сегодня</NavLink>
                 <NavLink to={'/all'} className={s.navLink} activeClassName={s.activeLink}>Все дни</NavLink>
-                Выбери вкладку (потом пофикшу)
             </nav>
             <Paper elevation={8} className={s.outputs} style={{backgroundColor: '#3f51b545'}}>
-                <Route path={'/'} exact render={() =>
-                    <Today/>}
-                />
-                <Route path={'/all'} exact render={() => <AllDays/>}/>
+                <Route exact path={'/'}>
+                    <Redirect to={'/today'} />
+                </Route>
+                <Route path={'/today'} render={() => <Today/>}/>
+                <Route path={'/all'} render={() => <AllDays/>}/>
             </Paper>
         </div>
     );
