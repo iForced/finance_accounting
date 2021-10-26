7cc42cee822fb6fd2bc722728552e99fd424887d
iForced <iforce@2ch.hk>
2021-10-26 21:30:31 +0300

    use HashRouter to fix routing on gh-pages

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './DataTable.module.css'
 import {Paper} from "@material-ui/core";
-import {NavLink, Redirect, Route} from "react-router-dom";
+import {NavLink, Route} from "react-router-dom";
 import Today from "./Today/Today";
 import AllDays from "./AllDays/AllDays";
 
@@ -10,15 +10,12 @@ const DataTable = () => {
     return (
         <div className={s.table}>
             <nav className={s.navbar}>
-                <NavLink to={'/today'} className={s.navLink} activeClassName={s.activeLink}>Сегодня</NavLink>
+                <NavLink to={'/'} exact className={s.navLink} activeClassName={s.activeLink}>Сегодня</NavLink>
                 <NavLink to={'/all'} className={s.navLink} activeClassName={s.activeLink}>Все дни</NavLink>
             </nav>
             <Paper elevation={8} className={s.outputs} style={{backgroundColor: '#3f51b545'}}>
-                <Route exact path={'/'}>
-                    <Redirect to={'/today'} />
-                </Route>
-                <Route path={'/today'} render={() => <Today/>}/>
-                <Route path={'/all'} render={() => <AllDays/>}/>
+                <Route path={'/'} exact render={() => <Today/>}/>
+                <Route path={'/all'} exact render={() => <AllDays/>}/>
             </Paper>
         </div>
     );
